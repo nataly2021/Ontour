e9e64fa2c716492fc2c8d83afaca5b3c2b4ee771
nataly <ch.lin@alumnos.duoc.cl>
2020-06-26 18:37:08 -0400

    Registre clientes pero no existe el telefono como atributo hay que eliminarlo

--- Persistencia/Views/WPF_RegistrarCliente.xaml.cs
+++ Persistencia/Views/WPF_RegistrarCliente.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Persistencia;
+using Servicios;
 
 namespace Views
 {
@@ -19,15 +21,98 @@ namespace Views
     /// </summary>
     public partial class WPF_RegistrarCliente : Window
     {
+        ServicioCliente sc = new ServicioCliente();
         public WPF_RegistrarCliente()
         {
             InitializeComponent();
         }
 
+        private void RegistrarCliente()
+        {
+            try
+            {
+                string rut;
+                string nombres;
+                string apellidos;
+                string mail;
+                string telefono;
+
+
+                if (txtrut.Text.Trim().Length > 0)
+                {
+                    rut = txtrut.Text;
+                }
+                else
+                {
+                    MessageBox.Show("Error:", "Debes ingresar un Rut válido");
+                    return;
+                }
+                if (txtnombre.Text.Trim().Length > 0)
+                {
+                    nombres = txtnombre.Text;
+                }
+                else
+                {
+                    MessageBox.Show("Error:", "Debes ingresar un nombre válido");
+                    return;
+                }
+                if (txtApellido.Text.Trim().Length > 0)
+                {
+                    apellidos = txtApellido.Text;
+                }
+                else
+                {
+                    MessageBox.Show("Error:", "Debes ingresar una Razon Social válida");
+                    return;
+                }
+                if (txtmail.Text.Trim().Length > 0)
+                {
+                    mail = txtmail.Text;
+                }
+                else
+                {
+                    MessageBox.Show("Error:", "Debes ingresar un mail válido");
+                    return;
+                }
+
+                if (txttelefono.Text.Trim().Length > 0)
+                {
+                    telefono = txttelefono.Text;
+                }
+                else
+                {
+                    MessageBox.Show("Error:", "Debes ingresar un numero de telefono válido");
+                    return;
+                }
+
+                CLIENTE cliente = new CLIENTE
+                {
+                    Rut_Cliente = rut,
+                    Nombre_Completo = nombres,
+                    Apellido_Completo = apellidos,
+                    Corre_Electronico = mail,
+
+
+                };
+                sc.AddEntity(cliente);
+                MessageBox.Show("Exito:", "Datos del autor registrados correctamente!!!");
+
+            }
+            catch (ArgumentException ex)
+            {
+                MessageBox.Show("Error:", ex.Message);
+            }
+
+        }
+
+
+
+
         private void Btn_aceptar_Click(object sender, RoutedEventArgs e)
         {
+            RegistrarCliente();
             MessageBox.Show("Cliente Agregado!!");
-            
+
         }
 
         private void Btn_atras_Cick(object sender, RoutedEventArgs e)
@@ -36,5 +121,5 @@ namespace Views
             window.Show();
             this.Close();
         }
-    }
+    }         
 }
